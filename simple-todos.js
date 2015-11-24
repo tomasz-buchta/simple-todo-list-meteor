8b0871910e1b84a1fa72313c6e90d8c5cad1520b
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-11-24 12:59:23 +0100

    Loading todos from db

--- simple-todos.js
+++ simple-todos.js
@@ -1,13 +1,13 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // counter starts at 0
   // Session.setDefault('counter', 0);
 
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1"},
-      { text: "This is task 2"},
-      { text: "This is task 3"}
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 
   Template.hello.events({
